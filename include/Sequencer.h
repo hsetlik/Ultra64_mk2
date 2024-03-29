fa1c68f805e9d26e7911462f17870d66dafa0a20
Hayden Setlik <haydensetlik@gmail.com>
2024-03-29 13:52:05 -0600

    timing and gate outputs

--- include/Sequencer.h
+++ include/Sequencer.h
@@ -7,78 +7,110 @@
 #include "Pins.h"
 #include "Sequence.h"
 
-//defaults and ranges
+// defaults and ranges
 #define TEMPO_DEFAULT 120
 #define TEMPO_MIN 25
 #define TEMPO_MAX 360
 
+#define OUTPUT_UPDATE_HZ 1000
+
 enum ButtonID
 {
-EncA,
-EncB,
-EncC,
-C1,
-C2,
-C3,
-C4,
-PL,
-PR
+    EncA,
+    EncB,
+    EncC,
+    C1,
+    C2,
+    C3,
+    C4,
+    PL,
+    PR
 };
 class Ultra64
 {
 public:
-Ultra64();
-~Ultra64();
-// call this in setup() to allocate everything and do setup stuff
-void init();
-// check the encoders and buttons, call this on a timer interrupt
-void pollInputs();
-// update the DAC and the gate outputs for the current state of the sequence, this also gets a timer interrupt
-void updateOutputs();
+    Ultra64();
+    ~Ultra64();
+    // call this in setup() to allocate everything and do setup stuff
+    void init();
+    // check the encoders and buttons, call this on a timer interrupt
+    void pollInputs();
+    // update the DAC and the gate outputs for the current state of the sequence, this also gets a timer interrupt at OUTPUT_UPDATE_HZ
+    void updateOutputs();
+
 private:
-// ---------- Hardware -------------
-Adafruit_NeoPixel* pixels;
+    // ---------- Hardware -------------
+    Adafruit_NeoPixel *pixels;
 
-RotaryEncoder* encA;
-RotaryEncoder* encB;
-RotaryEncoder* encC;
-long encAPos;
-long encBPos;
-long encCPos;
+    RotaryEncoder *encA;
+    RotaryEncoder *encB;
+    RotaryEncoder *encC;
+    long encAPos;
+    long encBPos;
+    long encCPos;
 
-Adafruit_MCP23X17 exp;
+    Adafruit_MCP23X17 exp;
 
-MCPButton* encAButton;
-MCPButton* encBButton;
-MCPButton* encCButton;
-MCPButton* c1;
-MCPButton* c2;
-MCPButton* c3;
-MCPButton* c4;
-MCPButton* pLeft;
-MCPButton* pRight;
-std::array<MCPButton*, 9> buttons = {encAButton, encBButton, encCButton, c1, c2, c3, c4, pLeft, pRight};
-//--------------------------------------
-// ---------- State Variables ----------
-uint16_t currentTempo = TEMPO_DEFAULT;
-uint8_t currentStep = 0;
-uint8_t selectedTrack = 0;
-uint8_t selectedStep = 0;
-bool isPlaying = false;
-bool quarterMode = false;
-bool lengthMode = false;
-Sequence seq;
-//--------------------------------------
-Step& getSelected() { return seq.tracks[selectedTrack][selectedStep]; }
-// control callback stuff-----------------------
-void encoderTurned(uint8_t enc, bool up);
-void buttonPressed(ButtonID id);
-void buttonHeld(ButtonID id);
+    MCPButton *encAButton;
+    MCPButton *encBButton;
+    MCPButton *encCButton;
+    MCPButton *c1;
+    MCPButton *c2;
+    MCPButton *c3;
+    MCPButton *c4;
+    MCPButton *pLeft;
+    MCPButton *pRight;
+    std::array<MCPButton *, 9> buttons = {encAButton, encBButton, encCButton, c1, c2, c3, c4, pLeft, pRight};
+    // helper function for the gate outputs
+    void setGate(uint8_t channel, bool level)
+    {
+        switch (channel)
+        {
+        case 0:
+            exp.digitalWrite(CH1, !level);
+            break;
+        case 1:
+            exp.digitalWrite(CH2, !level);
+            break;
+        case 2:
+            exp.digitalWrite(CH3, !level);
+            break;
+        case 3:
+            exp.digitalWrite(CH4, !level);
+            break;
+        default:
+            break;
+        }
+    }
+    void setCV(uint8_t channel, uint8_t midiNote)
+    {
+        // TODO
+    }
+    //--------------------------------------
+    // ---------- State Variables ----------
+    uint16_t currentTempo = TEMPO_DEFAULT;
+    uint8_t currentStep = 0;
+    uint8_t selectedTrack = 0;
+    uint8_t selectedStep = 0;
+    bool isPlaying = false;
+    bool quarterMode = false;
+    bool lengthMode = false;
+    Sequence seq;
+    //--------------------------------------
+    Step &getSelected() { return seq.tracks[selectedTrack][selectedStep]; }
+    // control callback stuff-----------------------
+    void encoderTurned(uint8_t enc, bool up);
+    void buttonPressed(ButtonID id);
+    void buttonHeld(ButtonID id);
 
-void shiftTempo(bool dir);
-void shiftSelectedStep(bool dir);
-void shiftStepNote(bool dir);
-void shiftStepLength(bool dir);
-//--------------------------------------
-
+    void shiftTempo(bool dir);
+    void shiftSelectedStep(bool dir);
+    void shiftStepNote(bool dir);
+    void shiftStepLength(bool dir);
+    //--------------------------------------
+    // helpers for timing math
+    const float sampleIntervalMs = 1000.0f / (float)OUTPUT_UPDATE_HZ;
+    float stepLengthMs = 60000.0f / (float)TEMPO_DEFAULT;
+    float msIntoSequence = 0.0f;
+    float msIntoStep = 0.0f;
 };
--- src/Sequencer.cpp
+++ src/Sequencer.cpp
@@ -1,24 +1,22 @@
 #include "Sequencer.h"
 
-Ultra64::Ultra64() : 
-pixels(nullptr),
-encA(nullptr),
-encB(nullptr),
-encC(nullptr),
-encAPos(0),
-encBPos(0),
-encCPos(0),
-encAButton(nullptr),
-encBButton(nullptr),
-encCButton(nullptr),
-c1(nullptr),
-c2(nullptr),
-c3(nullptr),
-c4(nullptr),
-pLeft(nullptr),
-pRight(nullptr)
+Ultra64::Ultra64() : pixels(nullptr),
+                     encA(nullptr),
+                     encB(nullptr),
+                     encC(nullptr),
+                     encAPos(0),
+                     encBPos(0),
+                     encCPos(0),
+                     encAButton(nullptr),
+                     encBButton(nullptr),
+                     encCButton(nullptr),
+                     c1(nullptr),
+                     c2(nullptr),
+                     c3(nullptr),
+                     c4(nullptr),
+                     pLeft(nullptr),
+                     pRight(nullptr)
 {
-
 }
 
 Ultra64::~Ultra64()
@@ -44,233 +42,245 @@ void Ultra64::init()
     // set pin modes
     pinMode(EXP_CS, OUTPUT);
     digitalWrite(EXP_CS, HIGH);
-    //set up SPI and initialize the MCP23S17 expander
+    // set up SPI and initialize the MCP23S17 expander
     SPI.begin(SCK, MOSI, MISO, EXP_CS);
-    if(!exp.begin_SPI(EXP_CS, &SPI, HW_ADDR))
+    if (!exp.begin_SPI(EXP_CS, &SPI, HW_ADDR))
     {
         Serial.println("Error! Failed to initialize IO expander!");
     }
 
     // buttons
     encAButton = new MCPButton(ENCA_B, &exp);
-    encAButton->setOnPress([this](){this->buttonPressed(EncA);});
-    encAButton->setOnHold([this](){this->buttonHeld(EncA);});
+    encAButton->setOnPress([this]()
+                           { this->buttonPressed(EncA); });
+    encAButton->setOnHold([this]()
+                          { this->buttonHeld(EncA); });
 
     encBButton = new MCPButton(ENCB_B, &exp);
-    encBButton->setOnPress([this](){this->buttonPressed(EncB);});
-    encBButton->setOnHold([this](){this->buttonHeld(EncB);});
+    encBButton->setOnPress([this]()
+                           { this->buttonPressed(EncB); });
+    encBButton->setOnHold([this]()
+                          { this->buttonHeld(EncB); });
 
     encCButton = new MCPButton(ENCC_B, &exp);
-    encCButton->setOnPress([this](){this->buttonPressed(EncC);});
-    encCButton->setOnHold([this](){this->buttonHeld(EncC);});
+    encCButton->setOnPress([this]()
+                           { this->buttonPressed(EncC); });
+    encCButton->setOnHold([this]()
+                          { this->buttonHeld(EncC); });
 
     c1 = new MCPButton(CH1, &exp);
-    c1->setOnPress([this](){this->buttonPressed(C1);});
-    c1->setOnHold([this](){this->buttonHeld(C1);});
+    c1->setOnPress([this]()
+                   { this->buttonPressed(C1); });
+    c1->setOnHold([this]()
+                  { this->buttonHeld(C1); });
 
     c2 = new MCPButton(CH2, &exp);
-    c2->setOnPress([this](){this->buttonPressed(C2);});
-    c2->setOnHold([this](){this->buttonHeld(C2);});
+    c2->setOnPress([this]()
+                   { this->buttonPressed(C2); });
+    c2->setOnHold([this]()
+                  { this->buttonHeld(C2); });
 
     c3 = new MCPButton(CH3, &exp);
-    c3->setOnPress([this](){this->buttonPressed(C3);});
-    c3->setOnHold([this](){this->buttonHeld(C3);});
+    c3->setOnPress([this]()
+                   { this->buttonPressed(C3); });
+    c3->setOnHold([this]()
+                  { this->buttonHeld(C3); });
 
     c4 = new MCPButton(CH4, &exp);
-    c4->setOnPress([this](){this->buttonPressed(C4);});
-    c4->setOnHold([this](){this->buttonHeld(C4);});
+    c4->setOnPress([this]()
+                   { this->buttonPressed(C4); });
+    c4->setOnHold([this]()
+                  { this->buttonHeld(C4); });
 
     pLeft = new MCPButton(P_LEFT, &exp);
-    pLeft->setOnPress([this](){this->buttonPressed(PL);});
-    pLeft->setOnHold([this](){this->buttonHeld(PL);});
+    pLeft->setOnPress([this]()
+                      { this->buttonPressed(PL); });
+    pLeft->setOnHold([this]()
+                     { this->buttonHeld(PL); });
 
     pRight = new MCPButton(P_RIGHT, &exp);
-    pRight->setOnPress([this](){this->buttonPressed(PR);});
-    pRight->setOnHold([this](){this->buttonHeld(PR);});
+    pRight->setOnPress([this]()
+                       { this->buttonPressed(PR); });
+    pRight->setOnHold([this]()
+                      { this->buttonHeld(PR); });
 
     // pixels
     pixels = new Adafruit_NeoPixel(24, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
     pixels->begin();
 
-    //encoders
-    encA = new RotaryEncoder(ENCA_L, ENCA_R, RotaryEncoder::LatchMode::TWO03); 
-    encB = new RotaryEncoder(ENCB_L, ENCB_R, RotaryEncoder::LatchMode::TWO03); 
-    encC = new RotaryEncoder(ENCC_L, ENCC_R, RotaryEncoder::LatchMode::TWO03); 
+    // encoders
+    encA = new RotaryEncoder(ENCA_L, ENCA_R, RotaryEncoder::LatchMode::TWO03);
+    encB = new RotaryEncoder(ENCB_L, ENCB_R, RotaryEncoder::LatchMode::TWO03);
+    encC = new RotaryEncoder(ENCC_L, ENCC_R, RotaryEncoder::LatchMode::TWO03);
 }
-
 
 void Ultra64::pollInputs()
 {
     // check encoders
     long aPos = encA->getPosition();
-    if(aPos != encAPos)
+    if (aPos != encAPos)
     {
         encoderTurned(0, aPos < encAPos);
         encAPos = aPos;
     }
 
     long bPos = encB->getPosition();
-    if(bPos != encBPos)
+    if (bPos != encBPos)
     {
         encoderTurned(1, bPos < encBPos);
         encBPos = bPos;
     }
 
     long cPos = encC->getPosition();
-    if(cPos != encCPos)
+    if (cPos != encCPos)
     {
         encoderTurned(2, cPos < encCPos);
         encCPos = cPos;
     }
-    //check buttons
-    for(auto* b : buttons)
+    // check buttons
+    for (auto *b : buttons)
     {
         b->tick();
     }
 }
 
 //===================================================================================
-void Ultra64::updateOutputs()
-{
-
-}
-
 
 void Ultra64::encoderTurned(uint8_t enc, bool dir)
 {
-    switch(enc)
+    switch (enc)
     {
-        case 0: // Left encoder: used to select the current step
-        {
-            shiftSelectedStep(dir);
-            break;
-        }
-        case 1: // Middle encoder: used to set the current pitch or length of the selected note
-        {
-            if(lengthMode)
-                shiftStepLength(dir);
-            else
-                shiftStepNote(dir);
-            break;
-        }
-        case 2: // Right encoder: used to change the tempo
-        {
-            shiftTempo(dir);
-            break;
-        }
-        default:
-            break;
+    case 0: // Left encoder: used to select the current step
+    {
+        shiftSelectedStep(dir);
+        break;
+    }
+    case 1: // Middle encoder: used to set the current pitch or length of the selected note
+    {
+        if (lengthMode)
+            shiftStepLength(dir);
+        else
+            shiftStepNote(dir);
+        break;
+    }
+    case 2: // Right encoder: used to change the tempo
+    {
+        shiftTempo(dir);
+        break;
+    }
+    default:
+        break;
     }
 }
 
 void Ultra64::buttonPressed(ButtonID id)
 {
-    switch(id)
+    switch (id)
     {
-        case EncA: // toggles note on/off
+    case EncA: // toggles note on/off
+    {
+        auto &s = getSelected();
+        s.gate = !s.gate;
+        break;
+    }
+    case EncB: // switches between note mode and length mode
+    {
+        lengthMode = !lengthMode;
+        break;
+    }
+    case EncC: // play/stop button
+    {
+        isPlaying = !isPlaying;
+        break;
+    }
+    case C1:
+    {
+        selectedTrack = 0;
+        break;
+    }
+    case C2:
+    {
+        selectedTrack = 1;
+        break;
+    }
+    case C3:
+    {
+        selectedTrack = 2;
+        break;
+    }
+    case C4:
+    {
+        selectedTrack = 3;
+        break;
+    }
+    case PL: // move selected step left one page
+    {
+        if (!isPlaying && !quarterMode)
         {
-            auto& s = getSelected();
-            s.gate = !s.gate;
-            break;
+            selectedStep = (selectedStep < 16) ? 48 + selectedStep : selectedStep - 16;
         }
-        case EncB: // switches between note mode and length mode
+        break;
+    }
+    case PR: // move right one page
+    {
+        if (!isPlaying && !quarterMode)
         {
-            lengthMode = !lengthMode;
-            break;
+            selectedStep = (selectedStep + 16) % 64;
         }
-        case EncC: // play/stop button
-        {
-            isPlaying = !isPlaying;
-            break;
-        }
-        case C1:
-        {
-            selectedTrack = 0;
-            break;
-        }
-        case C2:
-        {
-            selectedTrack = 1;
-            break;
-        }
-        case C3:
-        {
-            selectedTrack = 2;
-            break;
-        }
-        case C4:
-        {
-            selectedTrack = 3;
-            break;
-        }
-        case PL: // move selected step left one page
-        {
-            if(!isPlaying && !quarterMode)
-            {
-                selectedStep = (selectedStep < 16) ? 48 + selectedStep : selectedStep - 16;
-            }
-            break;
-        }
-        case PR: // move right one page
-        {
-            if(!isPlaying && !quarterMode)
-            {
-                selectedStep = (selectedStep + 16) % 64;
-            }
-            break;
-        }
-        default:
-            break;
+        break;
+    }
+    default:
+        break;
     }
 }
 
 void Ultra64::buttonHeld(ButtonID id)
 {
-    switch(id)
+    switch (id)
     {
-        case EncA: 
-        {
-            break;
-        }
-        case EncB:
-        {
-            break;
-        }
-        case EncC:
-        {
-            break;
-        }
-        case C1:
-        {
-            seq.clearTrack(0);
-            break;
-        }
-        case C2:
-        {
-            seq.clearTrack(1);
-            break;
-        }
-        case C3:
-        {
-            seq.clearTrack(2);
-            break;
-        }
-        case C4:
-        {
-            seq.clearTrack(3);
-            break;
-        }
-        case PL: // toggles quarter mode
-        {
-            quarterMode = !quarterMode;
-            break;
-        }
-        case PR:
-        {
-            break;
-        }
-        default:
-            break;
+    case EncA:
+    {
+        break;
+    }
+    case EncB:
+    {
+        break;
+    }
+    case EncC:
+    {
+        break;
+    }
+    case C1:
+    {
+        seq.clearTrack(0);
+        break;
+    }
+    case C2:
+    {
+        seq.clearTrack(1);
+        break;
+    }
+    case C3:
+    {
+        seq.clearTrack(2);
+        break;
+    }
+    case C4:
+    {
+        seq.clearTrack(3);
+        break;
+    }
+    case PL: // toggles quarter mode
+    {
+        quarterMode = !quarterMode;
+        break;
+    }
+    case PR:
+    {
+        break;
+    }
+    default:
+        break;
     }
 }
 
@@ -280,12 +290,14 @@ void Ultra64::shiftTempo(bool dir)
         currentTempo = std::min<uint16_t>(currentTempo + 1, TEMPO_MAX);
     else
         currentTempo = std::max<uint16_t>(currentTempo - 1, TEMPO_MIN);
+    // update the step length to match the new tempo
+    stepLengthMs = 60000.0f / (float)currentTempo;
 }
 
 void Ultra64::shiftSelectedStep(bool dir)
 {
     const uint8_t max = quarterMode ? 16 : 64;
-    if(dir)
+    if (dir)
     {
         selectedStep = (selectedStep + 1) % max;
     }
@@ -297,8 +309,8 @@ void Ultra64::shiftSelectedStep(bool dir)
 
 void Ultra64::shiftStepNote(bool dir)
 {
-    uint8_t& note = getSelected().midiNum;
-    if(dir)
+    uint8_t &note = getSelected().midiNum;
+    if (dir)
     {
         note = std::min<uint8_t>(note + 1, 127);
     }
@@ -310,8 +322,8 @@ void Ultra64::shiftStepNote(bool dir)
 
 void Ultra64::shiftStepLength(bool dir)
 {
-    uint8_t& length = getSelected().length;
-    if(dir)
+    uint8_t &length = getSelected().length;
+    if (dir)
     {
         length = std::min<uint8_t>(length + 1, 255);
     }
@@ -319,6 +331,30 @@ void Ultra64::shiftStepLength(bool dir)
     {
         length = std::max<uint8_t>(length - 1, LENGTH_MIN);
     }
-
+}
+//===================================================================================
+void Ultra64::updateOutputs()
+{
+    if (isPlaying)
+    {
+        // first we need to keep track of time
+        msIntoSequence += sampleIntervalMs;
+        msIntoStep += sampleIntervalMs;
+        if(msIntoStep > stepLengthMs) // time to advance to the next step
+        {
+            const uint8_t max = (quarterMode) ? 16 : 64;
+            currentStep = (currentStep + 1) % max;
+            msIntoStep -= stepLengthMs;
+            if(currentStep == 0)
+            {
+                msIntoSequence = msIntoStep;
+            }
+            // update the gates and control voltages
+            for(uint8_t ch = 0; ch < 4; ch++)
+            {
+                setGate(ch, seq.tracks[ch][currentStep].gate);
+            }
+        }
+    }
 }
 //===================================================================================
